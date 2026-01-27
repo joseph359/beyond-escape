f3949860eee1ebcc4fcd39ed2baeb03be8c6f5f2
joseph359 <josephys359@gmail.com>
2026-01-27 15:33:53 +0800

    fix base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "joseph359.github.io/beyond-escape",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
